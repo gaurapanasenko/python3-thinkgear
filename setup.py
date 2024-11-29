6ae8214c0326f0290b436e3cbea9879edd728e3e
Yehor Panasenko <yehor.panasenko@gmail.com>
2024-11-29 14:40:18 +0200

    Fixing pip

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="thinkgear-py3",
     version="2.0",
@@ -9,4 +15,6 @@ setup(
     description="ThinkGear Serial Stream Protocol implementation",
     url="https://github.com/gaurapanasenko/python3-thinkgear",
     install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
